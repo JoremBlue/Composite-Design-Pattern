77766a56a77dbddbb89c99f84b5e8c5191201116
Jorem Blue Bonador <135201717+JoremBlue@users.noreply.github.com>
2025-05-07 12:28:33 +0800

    Create NewEraUniversity.java

---
+++ NewEraUniversity.java
@@ -0,0 +1,33 @@
+public class NewEraUniversityDemo {
+    public static void main(String[] args) {
+        Student s1 = new Student("Alice", "S001", 20000);
+        Student s2 = new Student("Bob", "S002", 22000);
+        Student s3 = new Student("Charlie", "S003", 21000);
+
+        Teacher t1 = new Teacher("Dr. Smith", "Computer Science", 50000);
+        Teacher t2 = new Teacher("Dr. Jane", "Information Technology", 52000);
+
+        Department csDept = new Department("Computer Science");
+        csDept.addUnit(t1);
+        csDept.addUnit(s1);
+        csDept.addUnit(s2);
+
+        Department itDept = new Department("Information Technology");
+        itDept.addUnit(t2);
+        itDept.addUnit(s3);
+
+        College engCollege = new College("College of Engineering");
+        engCollege.addUnit(csDept);
+        engCollege.addUnit(itDept);
+
+        College mainUniversity = new College("New Era University");
+        mainUniversity.addUnit(engCollege);
+
+        System.out.println("--- University Structure ---");
+        mainUniversity.displayDetails();
+
+        System.out.println("\nTotal Students: " + mainUniversity.getStudentCount());
+
+        System.out.println("Total Budget: " + mainUniversity.getBudget());
+    }
+}
